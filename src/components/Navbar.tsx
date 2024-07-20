12bc3b9037884f59dec3e1eb149d79e731de27cd
Richard Olpindo <richardolpindo@Richards-Laptop.local>
2024-07-20 13:24:09 -1000

    Inputted the login provider in the app

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+export const Navbar = () => {
+    return (
+        <nav className="navbar-main">
+            <div className="navbar-left">
+                <h3>NOT MY TYPE</h3>  
+            </div>
+            <div className="navbar-right">
+                <a href="#">HOW TO PLAY</a>
+                
+            </div>
+
+        </nav>
+    )
+}
+    
--- src/providers/LoginProvider.tsx
+++ src/providers/LoginProvider.tsx
@@ -1,12 +1,32 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import { TUser, TLoginStatus } from "../Types";
 
 type TLoginProvider = {
-    
+    user: TUser | null;
+    loginStatus: TLoginStatus,
 }
 
 const LoginContext = createContext <TLoginProvider>({} as TLoginProvider)
 
-export const LoginProvider = ({children}:{children:ReactNode}) =>{
-    const [username, setUsername] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
+export const LoginProvider = ({children}:{children:ReactNode}) => {
+    const [user, setUser] = useState<TUser | null>(null);
+    const [loginStatus, setLoginStatus] = useState<TLoginStatus>('LoggedOut')
+
+    return (
+        <LoginContext.Provider value={{
+            user,
+            loginStatus
+        }}>
+            {children}
+        </LoginContext.Provider>
+    )
+}
+
+export const useLogin = () => {
+const context = useContext(LoginContext)
+
+    return {
+        user: context.user,
+        loginStatus: context.loginStatus
+    }
 }
