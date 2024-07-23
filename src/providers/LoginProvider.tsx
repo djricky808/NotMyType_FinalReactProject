f80ec2b074efb5e99f252aa4e98f44fcbf94596e
Richard Olpindo <djricky088@yahoo.com>
2024-07-23 08:17:48 -1000

    Added register, login, and logout to the Provider

--- src/providers/LoginProvider.tsx
+++ src/providers/LoginProvider.tsx
@@ -3,54 +3,66 @@ import { TUser, TLoginStatus } from "../Types";
 import { LoginRequests } from "../api/UserAPI";
 
 type TLoginProvider = {
-    user: TUser | null;
-    loginStatus: TLoginStatus,
-}
+  user: TUser | null;
+  loginStatus: TLoginStatus;
+  register: ({ username, password }: TUser) => Promise<void>;
+  login: ({ username, password }: TUser) => Promise<void>;
+  logout: () => void;
+};
 
-const LoginContext = createContext <TLoginProvider>({} as TLoginProvider)
+const LoginContext = createContext<TLoginProvider>({} as TLoginProvider);
 
-export const LoginProvider = ({children}:{children:ReactNode}) => {
-    const [user, setUser] = useState<TUser | null>(null);
-    const [loginStatus, setLoginStatus] = useState<TLoginStatus>('LoggedOut')
+export const LoginProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<TUser | null>(null);
+  const [loginStatus, setLoginStatus] = useState<TLoginStatus>("LoggedOut");
 
-    const register = ({username, password} : TUser) => {
-        return LoginRequests.registerFetch({username,password})
-        .then((user:TUser) => {
-            localStorage.setItem("user", JSON.stringify(user));
-            return setUser(user);
-        })
+  const register = ({ username, password }: TUser) => {
+    return LoginRequests.registerFetch({ username, password }).then(
+      (user: TUser) => {
+        localStorage.setItem("user", JSON.stringify(user));
+        return setUser(user);
+      }
+    );
+  };
+
+  const login = async ({ username, password }: TUser) => {
+    const user = await LoginRequests.getUserFromServer(username);
+    if (user.password !== password) {
+      throw new Error("Incorrect Password");
     }
+    setUser(user);
+    setLoginStatus("LoggedIn");
+  };
 
-    const login = async ({username, password}: TUser) => {
-        const user = await LoginRequests.getUserFromServer(username);
-        if(user.password !== password){
-            throw new Error('Incorrect Password')
-        }
-        setUser(user);
-        setLoginStatus('LoggedIn');
-    }
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    setLoginStatus("LoggedOut");
+  };
 
-    const logout = ()=> {
-        setUser(null);
-        localStorage.removeItem("user");
-        setLoginStatus("LoggedOut");
-    }
-
-    return (
-        <LoginContext.Provider value={{
-            user,
-            loginStatus
-        }}>
-            {children}
-        </LoginContext.Provider>
-    )
-}
+  return (
+    <LoginContext.Provider
+      value={{
+        user,
+        loginStatus,
+        register,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </LoginContext.Provider>
+  );
+};
 
 export const useLogin = () => {
-const context = useContext(LoginContext)
+  const context = useContext(LoginContext);
 
-    return {
-        user: context.user,
-        loginStatus: context.loginStatus
-    }
-}
+  return {
+    user: context.user,
+    loginStatus: context.loginStatus,
+    register: context.register,
+    login: context.login,
+    logout: context.logout,
+  };
+};
