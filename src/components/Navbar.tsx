a5adca6fa0f2ea894ffd79a446c8243cd260aa5f
Richard Olpindo <djricky088@yahoo.com>
2024-08-04 21:19:21 -1000

    Fixed CSS so that modals can be centered. Fixed how to play so that it closes all other open modals.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,25 @@
 import { useLogin } from "../providers/UseLogin";
+import { usePhrases } from "../providers/UsePhrases";
 import { useTutorial } from "../providers/UseTutorial";
 
 export const Navbar = () => {
   const { user, loginStatus, logout } = useLogin();
-  const {activateTutorial} = useTutorial()
-
+  const { activateTutorial } = useTutorial();
+  const { exitLevelModal } = usePhrases();
   return (
     <nav className="navbar-main">
       <div className="navbar-left">
         <h3>NOT MY TYPE</h3>
       </div>
       <div className="navbar-right">
-        <a onClick={() => activateTutorial()}>HOW TO PLAY</a>
+        <a
+          onClick={() => {
+            exitLevelModal();
+            activateTutorial();
+          }}
+        >
+          HOW TO PLAY
+        </a>
         {loginStatus === "LoggedIn" && (
           <a>
             Welcome <span>{user?.username}</span>
--- src/components/PhraseModal.tsx
+++ src/components/PhraseModal.tsx
@@ -1,7 +1,7 @@
 import { usePhrases } from "../providers/UsePhrases"
 
 export const PhraseModal = () => {
-    const {phraseLevel, exitLevelModal} = usePhrases();
+    const {allPhrases, phraseLevel, exitLevelModal} = usePhrases();
     const completedTime = null;
     const startGame = () => {
       console.log('New Game');
@@ -12,6 +12,7 @@ export const PhraseModal = () => {
         <div className="phrase-modal-scores">
           <div className="phrase-modal-left">
             <h1>{`Phrase ${phraseLevel}`}</h1>
+            <p>{allPhrases[phraseLevel-1].description}</p>
             <h2>Personal Best Time:</h2>
             {completedTime && <h1>{completedTime}</h1>}
             {!completedTime && <h1 className="incomplete">NOT COMPLETED</h1>}
